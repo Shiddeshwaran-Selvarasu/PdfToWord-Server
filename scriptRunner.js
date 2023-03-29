8bd7d36ba86a9b3ea1b9766aed41f426c009fe3f
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-29 12:13:29 +0530

    python

--- scriptRunner.js
+++ scriptRunner.js
@@ -8,7 +8,7 @@ const eventEmitter = new event.EventEmitter();
 
 async function converter(file) {
     log(`Converting file - ${path.join(process.cwd() + "/uploads", file.originalname)}`);
-    const converter = spawn('python3', ['./converter.py', path.join(process.cwd() + "/uploads", file.originalname)]);
+    const converter = spawn('sudo python3', ['./converter.py', path.join(process.cwd() + "/uploads", file.originalname)]);
     converter.stdout.on('data', function (data) {
         eventEmitter.emit('converted', JSON.parse(data));
     });

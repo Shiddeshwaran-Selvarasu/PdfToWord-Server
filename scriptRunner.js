2b3b5cc0de616fab933205922ee4789eccc5c2b9
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-29 13:30:59 +0530

    max listeners

--- scriptRunner.js
+++ scriptRunner.js
@@ -8,15 +8,18 @@ const eventEmitter = new event.EventEmitter();
 
 async function converter(file) {
     log(`Converting file - ${path.join(process.cwd() + "/uploads/" + file.originalname)}`);
-    const converter = childProcess.exec(`sudo python3 ./converter.py ${path.join(process.cwd() + "/uploads/" + file.originalname)}`);
-    converter.stdout.on('data', function (data) {
-        eventEmitter.emit('converted', JSON.parse(data));
-    });
-    converter.stdout.on('message', function (data) {
-        eventEmitter.emit('log', data.toString());
-    });
-    converter.on('close', (code) => {
-        eventEmitter.emit('closed', code);
+    const converter = childProcess.exec(`sudo python3 ./converter.py ${path.join(process.cwd() + "/uploads/" + file.originalname)}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(`stdout:\n${stdout}`);
     });
 }
 

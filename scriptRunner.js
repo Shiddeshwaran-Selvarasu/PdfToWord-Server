5fede5625cf5ff5be3f28f03cc88656a47991857
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-29 11:46:35 +0530

    python

--- scriptRunner.js
+++ scriptRunner.js
@@ -2,9 +2,12 @@ const { spawn } = require('child_process'); // spawn a child process to run the 
 const path = require('path'); // path module for file path handling
 const event = require('events'); // event module for event handling
 
+const log = require('./loger.js');
+
 const eventEmitter = new event.EventEmitter();
 
 async function converter(file) {
+    log(`Converting file - ${path.join(process.cwd() + "\\uploads\\", file.originalname)}`);
     const converter = spawn('python3', ['./converter.py', path.join(process.cwd() + "\\uploads\\", file.originalname)]);
     converter.stdout.on('data', function (data) {
         eventEmitter.emit('converted', JSON.parse(data));
